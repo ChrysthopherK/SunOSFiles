9e77ea3a21e40ae8e9156c05f7cbf0ed23d16ff7
ChrysthopherK <145856547+ChrysthopherK@users.noreply.github.com>
2023-10-21 18:02:09 -0300

    Update uninstall.js

--- SunOS/uninstall.js
+++ SunOS/uninstall.js
@@ -1,1 +1,15 @@
+const fs    = require('fs');
+const index = require('../../sunOS.js');
+/**
+ * File settings
+ */
+exports._isFileHidden = false;
 
+exports.evaluateFile = function (args) {
+  console.log(`Uninstalling system`);
+
+  var things = fs.readdirSync('./os');
+  things.forEach((file) => {
+    fs.rmSync(`./os/${file}`)
+  })
+};
